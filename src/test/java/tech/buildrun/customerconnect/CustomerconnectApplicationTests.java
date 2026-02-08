efa1b7bea4edb28ea3929ec717ece57911de6e38
Estefany Neres <estefanyyneres@gmail.com>
2026-02-08 17:19:52 -0300

    chore: configuracao£o inicial do proje

---
+++ src/test/java/tech/buildrun/customerconnect/CustomerconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.customerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
